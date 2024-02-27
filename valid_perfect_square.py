a4761aeb3c4844b71a10d7f6872fbe85830946b8
vctcpune2023 <vctcpune2023@gmail.com>
2024-02-27 17:54:36 +0530

    added another solution

--- valid_perfect_square.py
+++ valid_perfect_square.py
@@ -1,26 +1,36 @@
 
 
+class Solution:
+    def perfect_sqrt(self,num: int)-> bool:
+        "using the binary search approach "
+        low,high = 1, num//2
 
-def perfect_sqrt(num: int)-> bool:
-
-    
-    low,high = 1, num//2
-    
-
-    while(low <= high):
-        mid = (low + high) // 2
-        print(low,mid,high)
-        square = mid * mid
-        print(square)
-        if square == num:
-            return True
-        elif square < num:
-            low = mid+1
-            print(f"{low=}")
-        else:
-            high = mid-1
-            print(f"{high=}")
-    return False
+        while(low <= high):
+            mid = (low + high) // 2
+            square = mid * mid
+            if square == num:
+                return True
+            elif square < num:
+                low = mid+1
+            else:
+                high = mid-1  
+        return False
 
 
-print(perfect_sqrt(24))
+    def perfect_sqrt_(self, num:int) -> bool:
+        "using simple math function"
+        if num > 0:
+            sq_rt = int(num ** 0.5)
+            return sq_rt * sq_rt == num
+        
+
+sol = Solution()
+
+print(sol.perfect_sqrt(16))
+print(sol.perfect_sqrt(25))
+
+print(sol.perfect_sqrt_(16))
+print(sol.perfect_sqrt_(25))
+print(sol.perfect_sqrt_(15))
+
+
